2f940328b4426758d999a82e198f3d9d9cfd2722
dbasha <dudekula.basha@capgemini.com>
2022-07-23 22:04:14 +0530

    Initial Pages & Components

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+export { default as BodyPart } from './icons/body-part.png';
+export { default as Equipment } from './icons/equipment.png';
+export { default as Gym } from './icons/gym.png';
+export { default as LeftArrow } from './icons/left-arrow.png';
+export { default as RightArrow } from './icons/right-arrow.png';
+export { default as Target } from './icons/target.png';
+
+export { default as Banner } from './images/banner.png';
+export { default as Logo1 } from './images/Logo-1.png';
+export { default as Logo } from './images/Logo.png';
