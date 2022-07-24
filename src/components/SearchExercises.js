0653178f797fa655eef5e10b68c1e3f693c7fb4f
dbasha <dudekula.basha@capgemini.com>
2022-07-24 22:56:15 +0530

    FetchData util

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,45 @@
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
+import { HorizontalScrollbar } from './';
+
 const SearchExercises = () => {
-	const [search, setSearch] = useState('second');
+	const [search, setSearch] = useState('');
+	const [exercise, setExercise] = useState([]);
+	const [bodyParts, setBodyParts] = useState([]);
+
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			const bodyPartsData = await fetchData(
+				'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+				exerciseOptions
+			);
+
+			setBodyParts(['all', ...bodyPartsData]);
+		};
+
+		fetchExercisesData();
+	}, []);
 
 	const handleSearch = async () => {
 		if (search) {
-			// const excerciseData = await fetchData();
+			const exerciseData = await fetchData(
+				'https://exercisedb.p.rapidapi.com/exercises',
+				exerciseOptions
+			);
+
+			const searchedExercises = exerciseData.filter(
+				(exercise) =>
+					exercise.name.toLowerCase().includes(search) ||
+					exercise.bodyPart.toLowerCase().includes(search) ||
+					exercise.equipment.toLowerCase().includes(search) ||
+					exercise.target.toLowerCase().includes(search)
+			);
+
+			setSearch('');
+			setExercise(searchedExercises);
 		}
 	};
 
@@ -23,7 +56,12 @@ const SearchExercises = () => {
 			<Box position='relative' mb='72px'>
 				<TextField
 					sx={{
-						input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+						input: {
+							fontWeight: '700',
+							border: 'none',
+							borderRadius: '4px',
+							outline: 'none',
+						},
 						width: { lg: '800px', xs: '350px' },
 						backgroundColor: '#fff',
 						borderRadius: '40px',
@@ -52,6 +90,15 @@ const SearchExercises = () => {
 				>
 					Search
 				</Button>
+			</Box>
+			<Box
+				sx={{
+					position: 'relative',
+					width: '100%',
+					p: '20px',
+				}}
+			>
+				<HorizontalScrollbar data={bodyParts} />
 			</Box>
 		</Stack>
 	);
