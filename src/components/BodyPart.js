cee9263cc1a6f77c6b343abedb4ef83690302ae1
dbasha <dudekula.basha@capgemini.com>
2022-07-26 09:00:00 +0530

    BodyPart Component

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Stack, Typography } from '@mui/material';
 
-const BodyPart = () => {
-	return <div>BodyPart</div>;
+import { GymIcon } from '../assets';
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
+	return (
+		<Stack
+			type='button'
+			alignItems='center'
+			justifyContent='center'
+			className='bodyPart-card'
+			sx={{
+				borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+				backgroundColor: '#fff',
+				borderBottomLeftRadius: '20px',
+				width: '270px',
+				height: '282px',
+				cursor: 'pointer',
+				gap: '47px',
+			}}
+			onClick={() => {
+				setBodyPart(item);
+				window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+			}}
+		>
+			<img
+				src={GymIcon}
+				alt='gymicon'
+				style={{ width: '40px', height: '40px' }}
+			/>
+			<Typography
+				fontSize='24px'
+				fontWeight='bold'
+				color='#3A1212'
+				textTransform='capitalize'
+			>
+				{item}
+			</Typography>
+		</Stack>
+	);
 };
 
 export default BodyPart;
