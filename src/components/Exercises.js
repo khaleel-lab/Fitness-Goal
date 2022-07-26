fc23ea70f6e0894448453910321502a029b6ffb3
dbasha <dudekula.basha@capgemini.com>
2022-07-26 09:13:30 +0530

    Exercises Component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, Stack, Typography, Pagination } from '@mui/material';
 
-const Exercises = () => {
-	return <div>Exercises</div>;
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { ExerciseCard } from './';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+	return (
+		<Box id='exercises' sx={{ mt: { lg: '110px' } }} mt='50px' p='20px'>
+			<Typography variant='h3' mb='46px'>
+				Showing Results
+			</Typography>
+			<Stack
+				direction='row'
+				sx={{ gap: { lg: '110px', xs: '50px' } }}
+				flexWrap='wrap'
+				justifyContent='center'
+			>
+				{exercises.map((exercise, index) => (
+					<ExerciseCard key={index} exercise={exercise} />
+				))}
+			</Stack>
+		</Box>
+	);
 };
 
 export default Exercises;
