b624026725de3a409cf1c69fcf0e8ff88294b4c7
dbasha <dudekula.basha@capgemini.com>
2022-07-26 08:58:45 +0530

    Assests Icons name changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,8 @@
 export { default as BodyPartIcon } from './icons/body-part.png';
 export { default as EquipmentIcon } from './icons/equipment.png';
 export { default as GymIcon } from './icons/gym.png';
-export { default as LeftArrow } from './icons/left-arrow.png';
-export { default as RightArrow } from './icons/right-arrow.png';
+export { default as LeftArrowIcon } from './icons/left-arrow.png';
+export { default as RightArrowIcon } from './icons/right-arrow.png';
 export { default as TargetIcon } from './icons/target.png';
 
 export { default as Banner } from './images/banner.png';
